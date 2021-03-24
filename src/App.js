435ba27891213fc3dd1b867b7ba8322af5523639
reClicked <gamper.renato@gmail.com>
2021-03-24 19:53:00 +0100

    feature: set up routes for login and login google

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/auth/Login";
+import LoginGoogle from "./components/auth/LoginGoogle";
 
 function App() {
-  return <div className="App">lol </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Link to="/login">Login</Link>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/auth/google" component={LoginGoogle} />
+        </Switch>
+      </Router>{" "}
+    </div>
+  );
 }
 
 export default App;
