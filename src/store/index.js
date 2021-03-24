b9dc93364cdf41e5b9e88b317d06fec96fcf52c9
reClicked <gamper.renato@gmail.com>
2021-03-24 20:17:22 +0100

    feature: create redux store

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+import allReducer from "./reducers";
+
+const initialStates = {
+  auth: {
+    loggedIn: false,
+    user: {},
+  },
+};
+
+let store = createStore(
+  allReducer,
+  initialStates,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
