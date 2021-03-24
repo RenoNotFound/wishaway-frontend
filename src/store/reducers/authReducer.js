ac69a788687b18d96b76843d684a59166ff5e3a4
reClicked <gamper.renato@gmail.com>
2021-03-24 20:18:14 +0100

    feature: set up auth reducer for loggin in and out the user

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "SET_LOGIN":
+      return { ...state, loggedIn: true, user: action.payload };
+    case "SET_LOGOUT":
+      return { ...state, loggedIn: false, user: {} };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
