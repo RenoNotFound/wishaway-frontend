7df1528171e9695ccdbd62ca9eee51016bf49a25
reClicked <gamper.renato@gmail.com>
2021-03-31 17:40:46 +0200

    feature: add sign up route to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
 import LoginGoogle from "./components/auth/LoginGoogle";
 
 function App() {
@@ -9,8 +10,10 @@ function App() {
     <div className="App">
       <Router>
         <Link to="/login">Login</Link>
+        <Link to="/sign-up">Sign up</Link>
         <Switch>
           <Route exact path="/login" component={Login} />
+          <Route exact path="/sign-up" component={SignUp} />
           <Route exact path="/auth/google" component={LoginGoogle} />
         </Switch>
       </Router>{" "}
