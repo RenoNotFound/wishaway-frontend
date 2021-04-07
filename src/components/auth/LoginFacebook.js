2caefc753c432f228b7f7c4e678811fedcecbfc5
reClicked <gamper.renato@gmail.com>
2021-04-07 19:48:46 +0200

    feature: create facebook login component

---
+++ src/components/auth/LoginFacebook.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../store/actions";
+
+export default function LoginFacebook(props) {
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState(null);
+
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      await axios
+        .get(`/api/auth/facebook/callback${props.location.search}`, config)
+        .then((response) => {
+          console.log(response.data.data);
+          const data = response.data.data;
+          dispatch(login(data.user, data.token));
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setLoading(false);
+          setErrors(error);
+        });
+    };
+    getLoggedInUser();
+  }, [dispatch, props.location.search]);
+
+  if (loading) return <Fragment>Loading...</Fragment>;
+
+  return (
+    <div>
+      <details>
+        <summary>Welcome {auth.user.name}</summary>
+      </details>
+    </div>
+  );
+}
