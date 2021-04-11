5d0bef51a3c7dcef9d9f5852b56eaa74cc6294a6
reClicked <gamper.renato@gmail.com>
2021-04-11 20:28:20 +0200

    feature: create header for the app

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
+import LocalMallOutlinedIcon from "@material-ui/icons/LocalMallOutlined";
+import WishAwayLogo from "../../../assets/logo-wishaway.svg";
+
+import "./header.scss";
+
+export default function Header() {
+  return (
+    <header>
+      <section>
+        <div className="header-icon-container">
+          <SearchIcon className="header-icon" />
+          <p>search</p>
+        </div>
+
+        <div>
+          <Link to="/">
+            <img src={WishAwayLogo} alt="logo" width="100%" height="100%" />
+          </Link>
+        </div>
+        <div className="header-right">
+          <div className="header-icon-container">
+            <AccountCircleOutlinedIcon className="header-icon" />
+            <p>login / sign up</p>
+          </div>
+          <div className="header-icon-container bag">
+            <LocalMallOutlinedIcon className="header-icon" />
+            <p>bag</p>
+          </div>
+        </div>
+      </section>
+      <nav></nav>
+    </header>
+  );
+}
