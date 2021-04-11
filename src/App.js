bcff92f8b9c40d7fc95b03c9cece920d3475f1c3
reClicked <gamper.renato@gmail.com>
2021-04-11 20:30:52 +0200

    feature: wrap components around layout

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
 import LoginGoogle from "./components/auth/LoginGoogle";
 import LoginFacebook from "./components/auth/LoginFacebook";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Link to="/login">Login</Link>
-        <Link to="/sign-up">Sign up</Link>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/auth/google" component={LoginGoogle} />
-          <Route exact path="/auth/facebook" component={LoginFacebook} />
-        </Switch>
-      </Router>{" "}
+      <Layout>
+        <Router>
+          {/* <Link to="/login">Login</Link>
+          <Link to="/sign-up">Sign up</Link> */}
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/sign-up" component={SignUp} />
+            <Route exact path="/auth/google" component={LoginGoogle} />
+            <Route exact path="/auth/facebook" component={LoginFacebook} />
+          </Switch>
+        </Router>{" "}
+      </Layout>
     </div>
   );
 }
