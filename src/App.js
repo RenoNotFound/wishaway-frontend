6a6d197abf01722407f7dc04a0050a2abb79d305
reClicked <gamper.renato@gmail.com>
2021-04-13 14:53:55 +0200

    refactor: wrap all components with router

--- src/App.js
+++ src/App.js
@@ -6,23 +6,27 @@ import SignUp from "./components/auth/SignUp";
 import LoginGoogle from "./components/auth/LoginGoogle";
 import LoginFacebook from "./components/auth/LoginFacebook";
 import Layout from "./components/layout/Layout";
+import Products from "./components/products/Products";
+import Home from "./components/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Router>
+    <Router>
+      <div className="App">
+        <Layout>
           {/* <Link to="/login">Login</Link>
           <Link to="/sign-up">Sign up</Link> */}
           <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/products/:categoryId" component={Products} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/sign-up" component={SignUp} />
             <Route exact path="/auth/google" component={LoginGoogle} />
             <Route exact path="/auth/facebook" component={LoginFacebook} />
           </Switch>
-        </Router>{" "}
-      </Layout>
-    </div>
+        </Layout>
+      </div>
+    </Router>
   );
 }
 
