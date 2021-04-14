7533a55341d91569a5b65259529ce63f8b4ef6b7
reClicked <gamper.renato@gmail.com>
2021-04-14 10:11:49 +0200

    feature: add state for categories

--- src/components/layout/header/Navbar.js
+++ src/components/layout/header/Navbar.js
@@ -1,28 +1,13 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import api from "../../../services/ApiService";
-import { useDispatch, useSelector } from "react-redux";
-import { setCategories } from "../../../store/actions";
 
 export default function Navbar() {
-  const [loading, setLoading] = useState(true);
-
-  const categories = useSelector((state) => state.categories.categories);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    getCategories();
-  }, [dispatch]);
-
-  const getCategories = async () => {
-    const response = await api.getCategories();
-    if (typeof response !== undefined) {
-      dispatch(setCategories(response.data.categories));
-      setLoading(false);
-    }
-  };
-
-  if (loading) return <Fragment>Loading...</Fragment>;
+  const [categories] = useState([
+    { id: 1, name: "health" },
+    { id: 2, name: "electronics" },
+    { id: 3, name: "books" },
+    { id: 4, name: "fashion" },
+  ]);
 
   return (
     <nav>
@@ -30,7 +15,9 @@ export default function Navbar() {
         {categories.map((category) => {
           return (
             <li key={category.id}>
-              <Link to={`products/${category.id}`}>{category.name}</Link>
+              <Link to={`products/${category.id}/${category.name}`}>
+                {category.name}
+              </Link>
             </li>
           );
         })}
