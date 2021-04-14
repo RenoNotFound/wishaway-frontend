35c0bc12f0858f7a1910118f4c109736eb65fe33
reClicked <gamper.renato@gmail.com>
2021-04-14 10:09:00 +0200

    resource: delete react-router import

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
 
 export default function Layout(props) {
   return (
